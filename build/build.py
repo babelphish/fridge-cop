ff258bced770d939598ae271900414c5a9b00ecf
babelphish <babelphish@fridge-cop.com>
2014-05-25 17:36:45 -0400

    final build fixes

--- build/build.py
+++ build/build.py
@@ -5,7 +5,7 @@ config = configparser.ConfigParser()
 
 config.read("build.ini")
 build_number = int(config["version"]["build"])
-build_number += 1
+config["version"]["build"] = str(build_number + 1)
 
 with open('build.ini', 'w') as configfile:
     config.write(configfile)
@@ -14,7 +14,7 @@ f = open('..\consts.py', 'w')
 f.write("BUILD_NUMBER = " + str(build_number))
 f.close()
 
-subprocess.Popen(["java", "-jar", r"..\bin\compiler.jar", "--js", "jquery-1.11.1.min.js",
+process = subprocess.Popen(["java", "-jar", r"..\bin\compiler.jar", "--js", "jquery-1.11.1.min.js",
                 "--js", "vex.combined.min.js",
                 "--js", "main.js",
                 "--js", "json2.min.js",
@@ -27,3 +27,4 @@ subprocess.Popen(["java", "-jar", r"..\bin\compiler.jar", "--js", "jquery-1.11.1
                 "--compilation_level=SIMPLE",
                 "--js_output_file", "lib.min.js"], cwd=r"..\js"
                  )
+process.wait()
