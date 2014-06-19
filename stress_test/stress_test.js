ecf11ffa4f6bf8c6569491c966811aebe9819bbb
babelphish <babelphish@fridge-cop.com>
2014-06-18 23:35:58 -0400

    updated stress test to return basic data

--- stress_test/stress_test.js
+++ stress_test/stress_test.js
@@ -1,50 +1,101 @@
 var fs = require('fs');
 var clientFilePath = "../js/state_client.js";
 var clientFileContents = fs.readFileSync(clientFilePath).toString();
+var serverDateFormat = 'YYYY-MM-DD HH:mm:ss.SSS Z'
 eval(clientFileContents);
 
-var devUpdateURL = 'http://localhost:8081/';
+var moment = require('moment')
+
+var results = {};
+var instances = 500;
+var devUpdateURL = 'http://localhost:8081/state_changes';
+//var devUpdateURL = 'http://node.fridge-cop.com/state_changes';
+
 var io = require("socket.io-client");
+var heartbeatMilliseconds = 5 * 1000;
+var lastBroadcast = null;
 
-var socketURL = devUpdateURL;
-
-var clientId = 0;
-function instantiateClient()
+function instantiateNew(instance)
 {
-	clientId++;
-	console.log("Instantiated client:" + clientId);
-	new StateClient(global).init(socketURL, 
-		function onConnect()
+	results[instance] = {};
+	results[instance].startTryConnectTime = moment();
+	var socket = io.connect(devUpdateURL, {"forceNew" : true});
+
+	socket.on('connect', function()
+	{
+		results[instance].connectTime = moment();
+		console.log("Instance " + instance + " connected.")
+		socket.on('new_states', function (data)
 		{
-			console.log('connect');
-		},
-		function onReconnect()
-		{
-			console.log('reconnect');
-			/*
-			$.get("/current_state").done(function(data)
+			if (instance == 1)
 			{
-				new StateData(JSON.parse(data)).apply()
-			})
-			*/
-		},
-		function onStateChange(data)
+				lastBroadcast = data;
+			}
+			results[instance].messageTime = moment();
+		});
+
+		socket.on('disconnect', function()
 		{
-			console.log('state change');
-			//processState(JSON.parse(data));
-		},
-		function onDisconnect()
-		{
-			console.log("disconnect");
-			//setFridgeStateUnknown();
-		}
-	)
+		});
+	});
 }
 
-instantiateClient();
+function getInstance(i)
+{
+	return function()
+	{
+		instantiateNew(i);		
+	}
+}
+
+for (var i = 0; i < instances; i++)
+{		
+	setTimeout(getInstance(i), parseInt((heartbeatMilliseconds / instances) * i));
+}
 
 var app = require("express")();
 app.listen(8082, "127.0.0.1");
-app.get("/dump", function(req, res) {
-  res.send("test");
+app.get("/dump", function(req, res)
+{
+	var output = "";
+	var lastTime = moment(JSON.parse(lastBroadcast).t, serverDateFormat);
+	output += lastTime.format();
+	var connectionTimes = [];
+	var messageTimes = [];
+	for (var i = 0; i < instances; i++)
+	{
+		messageTimes.push(results[i].messageTime)
+		connectionTimes.push(results[i].connectTime.diff(results[i].startTryConnectTime))
+	}
+	connectionTimes.sort(function(a,b) 
+	{
+		return a - b;
+	});
+	messageTimes.sort(function(a,b)
+	{
+		return a.diff(b);
+	});
+
+	var min = messageTimes[0];
+	var max = messageTimes[messageTimes.length - 1];
+	
+	var connectionTimesMean = 0;
+	for (var i = 0; i < instances; i++)
+	{
+		connectionTimesMean += connectionTimes[i];
+	}
+	connectionTimesMean /= connectionTimes.length;
+
+	var messageTimesMean = 0;
+	for (var i = 0; i < instances; i++)
+	{
+		var difference = messageTimes[i].diff(min)
+		messageTimesMean += difference;
+	}
+	messageTimesMean /= messageTimes.length
+
+	output += "Mean time to connect: " + connectionTimesMean + " ms<br>";
+	output += "Mean time to transmit message: " + messageTimesMean + " ms<br>";
+	
+	res.send(output)
 });
